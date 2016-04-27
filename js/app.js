6bed2ce64ec0fa1f002635c92d581dd27bd986cc
Carly Williams <carly.kurth@gmail.com>
2016-04-27 01:05:45 -0700

    Add functionality.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,14 @@
 $(document).ready( function() {
 
+	var resultMovies = [];
+	var inputMovies = [];
 
-	$(".btn-search").click(function(){
+	$('.btn-search').click(function(){
 		$('.movie-entered').html('');
+		$('.movie-results').html('');
+		$('.movie-teaser').text('');
+		$('.movie-youtube').html('');
+		$(".txt-label").css("display", "none");
 		var input_movie = $(".input-query").val();
 		getRecommendations(input_movie);
 	});
@@ -11,9 +17,31 @@ $(document).ready( function() {
 		if (event.keyCode === 13) {
 			event.preventDefault();
 			$('.movie-entered').html('');
+			$('.movie-results').html('');
+			$('.movie-teaser').text('');
+			$('.movie-youtube').html('');
+			$(".txt-label").css("display", "none");
 			var input_movie = $(".input-query").val();
 			getRecommendations(input_movie);
 		}
+	});
+
+	$('.movie-results').on('click', '.movie-title', function(event){
+		var num = $(this).attr("result-num");
+		var teaser = resultMovies[num].wTeaser;
+		var video = resultMovies[num].yUrl;
+		$('.movie-teaser').text(teaser);
+		var embeddedVideo = '<iframe title="YouTube video player" class="youtube-player" type="text/html" width="450" height="330" src="' + video + '" frameborder="0" allowFullScreen></iframe>';
+		$('.movie-youtube').html(embeddedVideo);
+	});
+
+	$('.movie-entered').on('click', '.movie-title', function(event){
+		var num = $(this).attr("result-num");
+		var teaser = inputMovies[num].wTeaser;
+		var video = inputMovies[num].yUrl;
+		$('.movie-teaser').text(teaser);
+		var embeddedVideo = '<iframe title="YouTube video player" class="youtube-player" type="text/html" width="450" height="330" src="' + video + '" frameborder="0" allowFullScreen></iframe>';
+		$('.movie-youtube').html(embeddedVideo);
 	});
 
 
@@ -21,36 +49,37 @@ $(document).ready( function() {
 // this function takes the answerer object returned by the StackOverflow request
 // and returns new result to be appended to DOM
 
-	function showInputMovies(info) {
+	function showInputMovies(info, num) {
 		// clone our input-movie template code
 		var result = $('.templates .input-movie').clone();
 
 		// Set the movie-title properties in result
 		var nameElem = result.find('.movie-title');
 		nameElem.text(info.Name);
+		nameElem.attr("result-num", num);
 
 		return result;
 	}
 
-	function showMovieResults(results) {
+	function showMovieResults(results, num) {
 		// clone our result template code
 		var result = $('.templates .result-movie').clone();
 
 		// Set the movie-title properties in result
 		var nameElem = result.find('.movie-title');
 		nameElem.text(results.Name);
+		nameElem.attr("result-num", num);
 
 		return result;
 	}
 
 	function getRecommendations(movie_name) {
-		// the parameters we need to pass in our request to StackOverflow's API
+		// the parameters we need to pass in our request to TasteKid's API
 		var request = { 
 			q: movie_name,
 			type: 'movies',
 			k: '220566-Thinkful-Z65T1U0T',
 			verbose: 1
-			//callback: getData
 		};	
 
 		$.ajax({
@@ -64,30 +93,33 @@ $(document).ready( function() {
 			//$.each is a higher order function. It takes an array and a function as an argument.
 			//The function is executed once for each item in the array.
 			$.each(result.Similar.Info, function(i, item) {
-				var movie = showInputMovies(item);
-				$('.movie-entered').append(movie);
+				if (item.Type === 'unknown') {
+					$('.movie-entered').html("<div class=\"no-info\">We're sorry! We don't have any info on '" + item.Name +"'.</div>");
+				} 
+				else {
+					$(".txt-label").css("display", "block");
+					inputMovies[i] = {};
+					inputMovies[i].Name = item.Name;
+					inputMovies[i].wTeaser = item.wTeaser;
+					inputMovies[i].yUrl = item.yUrl;
+					var movie = showInputMovies(item, i);
+					$('.movie-entered').append(movie);
+				}
 			});
 			$.each(result.Similar.Results, function(i, item) {
-				var movie = showMovieResults(item);
+				resultMovies[i] = {};
+				resultMovies[i].Name = item.Name;
+				resultMovies[i].wTeaser = item.wTeaser;
+				resultMovies[i].yUrl = item.yUrl;
+				var movie = showMovieResults(item, i);
 				$('.movie-results').append(movie);
-			});			
+			});		
 		})
 		.fail(function(jqXHR, error){ //this waits for the ajax to return with an error promise object
 			console.log("fail");
 			// var errorElem = showError(error);
 			// $('.search-results').append(errorElem);
 		});
-
-		// function getData(data) {
-  //     if (data.ok) {
-  //           // do something with the data here
-  //           console.log("OK");
-  //       } else {
-  //           alert(data.error);
-  //           console.log("error");
-  //     }
-		// }
-
 	}
 
 });
